2661a43b6465475d28aa1ee54fdb1905f72104f0
vikas-843 <119723483+vikas-843@users.noreply.github.com>
2024-01-08 18:03:30 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,9 +1,9 @@
 API_ID       = 25826048
 API_HASH     = "b486ee260537697fdfc56b2b61cbc049"
-BOT_TOKEN    = "6374070501:AAGyxXJvxQneKm0KzEyLVr9SYUn67gZROyo"
+BOT_TOKEN    = "6721499878:AAHlp2jWvhwOJpa2VezO1llYrAEoB5mhnaw"
 SESSION      = "BQBiMZkAPG4cnQS9tq2V0VxVAlFgveNVpLxNIs2BVG0yaSHSmEoDQJP3v4gdjYU8I1lpZlAQfVrTOiOGkpChOlIM1I4hxEVWcDcuQpJ_l2tJ8WmwCfV78mIBsr2I5_zn0GqS7yeFHIcRgJvVLIrhS_saUjDCST4zRNUysCX-NYdsO7fOrBdAyFeTwXiK5w4543cF5jlZkDQZOrxq5jqNqeAKWeZQpUaVUdeBfLMPvDd061QZgb2n6cZOMr0q_wbggaXfYpBmL3LXDJO7zdZw4pwZE1uL4poxyiqIoEPUmgZ1pzqoHNcQGK1QeGBYAfRR6PCHoNxuQor8_EUTo1sTC778fYnoAQAAAAGW8Ud6AA"
 DATABASE_URI = "mongodb+srv://bikashxd:bgtop@cluster0.gh1hqe3.mongodb.net/?retryWrites=true&w=majority"
-LOG_CHANNEL  = -1002126379861
+LOG_CHANNEL  = -1001875834087
 ADMIN        = 6324330202
 CHANNEL      = "@GreyMatter_Bots"
 MONGO_URI = "mongodb+srv://vikas:vikas@vikas.yfezexk.mongodb.net/?retryWrites=true&w=majority"
